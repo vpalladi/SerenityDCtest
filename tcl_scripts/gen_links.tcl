6be1eb7877d04e153503511ae8fe751770904445
vpalladi <v.palladino@imperial.ac.uk>
2020-01-14 18:30:48 +0100

    gen_links done.

--- tcl_scripts/gen_links.tcl
+++ tcl_scripts/gen_links.tcl
@@ -25,7 +25,7 @@ package require json
 
 # GTY and GTH "numbering"
 
-set MGTtypeId [ dict create GTH "_1_0_0/IBERT" GTY "_1_0_50/IBERT" ]
+set MGTtypeId [ dict create GTH "_1_0_0/IBERT" GTY "_1_0_55/IBERT" ]
 
 # open files
 set tclBase  "/home/hwtest/vpalladi/SerenityDCtest/tcl_scripts"
@@ -53,7 +53,7 @@ set JTAG        [ dict get $config JTAG ]
 set JTAGid      [ dict get $JTAG JTAGid ]
 set host        [ dict get $JTAG host ]
 
-set pathBase "$host/xilinx_tcf/$JTAGid/"
+set pathBase "$host/xilinx_tcf/$JTAGid"
 
 set connections [ dict get $config connections ]
 
@@ -74,9 +74,10 @@ dict for { id c } $connections {
     set daughterCardB [ dict get $DCs $siteB ]
     set socketBtmp    [ dict get [dict get $connectivity [dict get $daughterCardB type] ] $connectorB] 
     
+    ## invert lines on the fiber/bundle if needed
     set socketB []
     set nConnections [dict size $socketBtmp]
-    if { $connectionType == "fiber" } {
+    if { $connectionType == "Cu8" || $connectionType == "Cu12" } {
         
         for {set i [expr $nConnections-1]} {$i > -1} {incr i -1} { 
             dict set socketB $i [dict get $socketBtmp $i] 
@@ -140,150 +141,60 @@ dict for { id c } $connections {
         set rxMGTid [dict get $MGTtypeId $rxMGTtype ]
         set txMGTid [dict get $MGTtypeId $txMGTtype ]
 
-        set txPath "$pathBase/$JTAGid/$txSite$txMGTid"
-        set rxPath "$pathBase/$JTAGid/$rxSite$rxMGTid"
+        set txPath "$pathBase/$txSite$txMGTid/$txMGT"
+        set rxPath "$pathBase/$rxSite$rxMGTid/$rxMGT"
+
+        set description "Link X$txSite-$txMGT -> X$rxSite-$rxMGT"
+        puts $description
         
-        set description "Link tx$txMGT rx$txMGT "
-        set rxPath "$rxPath/$rxMGT"
-        set txPath "$txPath/$txMGT"
+        # generate the link
+        set xil_newLink [create_hw_sio_link -description $description [lindex [get_hw_sio_txs $txPath] 0] [lindex [get_hw_sio_rxs $rxPath] 0] ]
+        lappend xil_newLinks $xil_newLink
         
-        puts $txPath
-        puts $rxPath
-        
-        #set xil_newLink [create_hw_sio_link -description $description [lindex [get_hw_sio_txs $txPath] 0] [lindex [get_hw_sio_rxs $rxPath] 0] ]
-        puts [lindex [get_hw_sio_txs $txPath] 0] 
-        puts [lindex [get_hw_sio_rxs $rxPath] 0]
-        #lappend xil_newLinks $xil_newLink
-        
-    }
-    
-    #set groupDescription "$baseBoard\_site$siteId\_DC$DCid\_$connectionType:Tx$txId-Rx$rxId"
-    #puts $groupDescription
-    #set xil_newLinkGroup [create_hw_sio_linkgroup -description $groupDescription [get_hw_sio_links $xil_newLinks]]
-    #unset xil_newLinks
-    
-    
     }
 
-#dict for { siteId DC } $DCs {
-#
-#    set DCid [ dict get $DC id ]
-#
-#    if { [string compare -nocase $DCid "none"] != 0 } {
-# 
-#        puts "DC $DCid on site: $siteId"
-#
-#        set connections [ dict get $DC connections ]
-#        set pathBaseGTY [ dict get [ dict get $data pathBaseGTY ] DC$siteId ]
-#        set pathBaseGTH [ dict get [ dict get $data pathBaseGTH ] DC$siteId ]
-#        
-#        dict for {cId connection} $connections {
-#            
-#            set txId           [ dict get $connection Tx ]
-#            set rxId           [ dict get $connection Rx ]
-#            set connectionType [ dict get $connection type ]
-#            
-#            set txList [ dict get [ dict get $data tx$txId ] MGTs ]
-#            set rxTmpL [ dict get [ dict get $data rx$rxId ] MGTs ]
-#            
-#            set rxList []
-#            if { $connectionType == "fiber" } {
-#                
-#                for {set i 11} {$i > -1} {incr i -1} { 
-#                    dict set rxList $i [dict get $rxTmpL $i] 
-#                }
-#                
-#            } else {
-#                
-#                for {set i 0} {$i < 12} {incr i} { 
-#                    dict set rxList $i [dict get $rxTmpL $i] 
-#                }
-#                
-#            } 
-#            
-#            set xil_newLinks [ list ]
-#            set i 0
-#            
-#            foreach {rxMGTid rx} $rxList {txMGTid tx} $txList {
-#                
-#                # get the MGTs path and type
-#                set rxMGT [ dict get $rx MGT ]
-#                set txMGT [ dict get $tx MGT ]
-#                set rxMGTtype [ dict get $rx type ]
-#                set txMGTtype [ dict get $tx type ]
-#                set rxPath []
-#                set txPath []
-#                
-#                if { $rxMGTtype == "GTY" } {
-#                    set rxPath [ dict get [ dict get $data pathBaseGTY ] DC$siteId ]
-#                } else {
-#                    set rxPath [ dict get [ dict get $data pathBaseGTH ] DC$siteId ]
-#                }
-#                
-#                if { $txMGTtype == "GTY" } {
-#                    set txPath [ dict get [ dict get $data pathBaseGTY ] DC$siteId ]
-#                } else {
-#                    set txPath [ dict get [ dict get $data pathBaseGTH ] DC$siteId ]
-#                }
-#                
-#                set description "Link tx$txMGTid rx$rxMGTid"
-#                set rxPath "$rxPath/$rxMGT"
-#                set txPath "$txPath/$txMGT"
-#            
-#                puts $txPath
-#                puts $rxPath
-#
-#                set xil_newLink [create_hw_sio_link -description $description [lindex [get_hw_sio_txs $txPath] 0] [lindex [get_hw_sio_rxs $rxPath] 0] ]
-#                lappend xil_newLinks $xil_newLink
-#                
-#            }
-#            
-#            set groupDescription "$baseBoard\_site$siteId\_DC$DCid\_$connectionType:Tx$txId-Rx$rxId"
-#            puts $groupDescription
-#            set xil_newLinkGroup [create_hw_sio_linkgroup -description $groupDescription [get_hw_sio_links $xil_newLinks]]
-#            unset xil_newLinks
-#            
-#    }
-#    } else {
-#        puts "No DC on site: $siteId"
-#        
-#    }
-#    
-#}
-#
-#### setup links
-#set links [get_hw_sio_links]
-#
-#foreach link $links {
-#
-#    # DFE off
-#    #set_property RXDFEENABLED {0} [get_hw_sio_links $link]
-#    #set_property TXDFEENABLED {0} [get_hw_sio_links $link]
-#    
-#    # PRBS set to 31 bits
-#    set_property TX_PATTERN {PRBS 31-bit} [get_hw_sio_links $link]
-#    set_property RX_PATTERN {PRBS 31-bit} [get_hw_sio_links $link]
-#
-#    # set polarity only if status is NO LINK
-#    set linkStatus [ get_property STATUS [get_hw_sio_links $link] ]
-#    if { $linkStatus == "NO LINK" } {
-#        set_property PORT.RXPOLARITY {1} [get_hw_sio_links $link]
-#        commit_hw_sio [get_hw_sio_links $link]
-#    }
-#
-#    # rx reset
-#    set_property LOGIC.RX_RESET_DATAPATH 1 [get_hw_sio_links $link]
-#    commit_hw_sio [get_hw_sio_links $link]
-#    set_property LOGIC.RX_RESET_DATAPATH 0 [get_hw_sio_links $link]
-#    commit_hw_sio [get_hw_sio_links $link]
-#    
-#    # reset counters
-#    set_property LOGIC.MGT_ERRCNT_RESET_CTRL 1 [get_hw_sio_links $link]
-#    commit_hw_sio [get_hw_sio_links $link]
-#    set_property LOGIC.MGT_ERRCNT_RESET_CTRL 0 [get_hw_sio_links $link]
-#    commit_hw_sio [get_hw_sio_links $link]
-#
-#}
+    # group links
+    set groupDescription "$baseBoard\_$connectionType:$siteA\_$connectorA-$siteB\_$connectorB"
+    puts $groupDescription
+    set xil_newLinkGroup [create_hw_sio_linkgroup -description $groupDescription [get_hw_sio_links $xil_newLinks]]
+    unset xil_newLinks
+    
+    
+}
+
+### setup links
+set links [get_hw_sio_links]
+
+foreach link $links {
+
+    # DFE off
+    #set_property RXDFEENABLED {0} [get_hw_sio_links $link]
+    #set_property TXDFEENABLED {0} [get_hw_sio_links $link]
+    
+    # PRBS set to 31 bits
+    set_property TX_PATTERN {PRBS 31-bit} [get_hw_sio_links $link]
+    set_property RX_PATTERN {PRBS 31-bit} [get_hw_sio_links $link]
+
+    # set polarity only if status is NO LINK
+    set linkStatus [ get_property STATUS [get_hw_sio_links $link] ]
+    if { $linkStatus == "NO LINK" } {
+        set_property PORT.RXPOLARITY {1} [get_hw_sio_links $link]
+        commit_hw_sio [get_hw_sio_links $link]
+    }
+
+    # rx reset
+    set_property LOGIC.RX_RESET_DATAPATH 1 [get_hw_sio_links $link]
+    commit_hw_sio [get_hw_sio_links $link]
+    set_property LOGIC.RX_RESET_DATAPATH 0 [get_hw_sio_links $link]
+    commit_hw_sio [get_hw_sio_links $link]
+    
+    # reset counters
+    set_property LOGIC.MGT_ERRCNT_RESET_CTRL 1 [get_hw_sio_links $link]
+    commit_hw_sio [get_hw_sio_links $link]
+    set_property LOGIC.MGT_ERRCNT_RESET_CTRL 0 [get_hw_sio_links $link]
+    commit_hw_sio [get_hw_sio_links $link]
+
+}
 
 
 
